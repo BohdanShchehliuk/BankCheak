72e06e8cd0d5e2b98e6e081c804a5157908336f1
shchehlyuk-b-p <shbp87@gmail.com>
2021-11-17 09:05:28 +0200

    Дивно, але воно працює)))

---
+++ com/company/EnterInformation.java
@@ -0,0 +1,69 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class EnterInformation {
+    private String birthday;
+    private String secretAsk;
+    private String secretAnsver;
+    private String email;
+    private String lastOrderNumber;
+    private String favoriteColour;
+
+    public EnterInformation() {
+        System.out.println("Введіть день народження");
+        Scanner input = new Scanner(System.in);
+        String birthday = input.nextLine();
+        this.birthday = birthday;
+        input = new Scanner(System.in);
+
+        System.out.println("Введіть email");
+        String email = input.nextLine();
+        this.email = email;
+        input = new Scanner(System.in);
+
+        System.out.println("Введіть secretAsk");
+        String secretAsk = input.nextLine();
+        this.secretAsk = secretAsk;
+        input = new Scanner(System.in);
+
+        System.out.println("Введіть secretAnsver");
+        String secretAnsver = input.nextLine();
+        this.secretAsk = secretAnsver;
+        input = new Scanner(System.in);
+
+        System.out.println("Введіть lastOrderNumber");
+        String lastOrderNumber = input.nextLine();
+        this.lastOrderNumber = lastOrderNumber;
+        input = new Scanner(System.in);
+
+        System.out.println("Введіть lastOrderNumber");
+        String favoriteColour = input.nextLine();
+        this.favoriteColour = favoriteColour;
+        input = new Scanner(System.in);
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public String getSecretAsk() {
+        return secretAsk;
+    }
+
+    public String getSecretAnsver() {
+        return secretAnsver;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getLastOrderNumber() {
+        return lastOrderNumber;
+    }
+
+    public String getFavoriteColour() {
+        return favoriteColour;
+    }
+}
---
+++ com/company/UserLoginPassword.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class UserLoginPassword {
+    private String loginRegistration = "BohdanSh"; //45% loginRegistration - ЛОГІН
+    private String password = "1@#2"; //45% password - ПАРЛЬ
+
+    public String getLoginRegistration() {
+        return loginRegistration;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
+
