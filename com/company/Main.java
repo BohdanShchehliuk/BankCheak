2a0bf8894bedfc123001950e0c6b7bf1a0972856
shchehlyuk-b-p <shbp87@gmail.com>
2021-11-17 08:15:20 +0200

    Initial commit

---
+++ com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        UserLoginPassword userLoginPassword = new UserLoginPassword();
+        EnterLoginPassword enterLoginPassword = new EnterLoginPassword();
+        if (userLoginPassword.getLoginRegistration().equals(enterLoginPassword.getEnterLogin()) &&
+                userLoginPassword.getPassword().equals(enterLoginPassword.getEnterPassword())) {
+            System.out.println("Welcome");
+        } else {
+            System.out.println("Sorry. Enter is forbiden");
+        }
+
+
+        // write your code here
+    }
+}
